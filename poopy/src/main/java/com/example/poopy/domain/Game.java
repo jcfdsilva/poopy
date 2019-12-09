5a3d16d150f6363fee03a415417039645f8a0301
jcfdsilva <jcfdsilva@gmail.com>
2019-12-09 11:44:39 +0200

    test blank space

--- poopy/src/main/java/com/example/poopy/domain/Game.java
+++ poopy/src/main/java/com/example/poopy/domain/Game.java
@@ -12,8 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
